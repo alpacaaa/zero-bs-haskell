793225b5a419d0d7c928f4312261bd208ee1f88d
Marco Sampellegrini <babbonatale@alpacaaa.net>
2019-03-06 19:08:23 +0000

    Start exercises from CLI.

--- exercises/Main.hs
+++ exercises/Main.hs
@@ -1,10 +1,16 @@
 
--- import qualified Ex01StaticString.StaticString
--- import qualified Ex02Echo.Echo
+import qualified Ex01StaticString.StaticString
+import qualified Ex02Echo.Echo
 import qualified Ex03CaseMatch.CaseMatch
 
+import qualified System.Environment
+
 main :: IO ()
-main
-  -- = Ex01StaticString.StaticString.main
-  -- = Ex02Echo.Echo.main
-  = Ex03CaseMatch.CaseMatch.main
+main = do
+  n <- System.Environment.getArgs
+  case n of
+    ["1"] -> Ex01StaticString.StaticString.main
+    ["2"] -> Ex02Echo.Echo.main
+    ["3"] -> Ex03CaseMatch.CaseMatch.main
+    _     ->
+      putStrLn "Invalid exercise number. Provide number as a cli argument."
